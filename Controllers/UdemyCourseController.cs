76828971d05c62c6983fa0901dd8b527c9df87cc
DirleaAndrei <78161390+DirleaAndrei@users.noreply.github.com>
2021-09-04 16:06:43 +0300

    Added ConvertResponseToUdemyCourse method

--- Controllers/UdemyCourseController.cs
+++ Controllers/UdemyCourseController.cs
@@ -19,14 +19,12 @@ namespace team2backend.Controllers
         public IEnumerable<UdemyCourse> Get(string searchFor, int numPages)
         {
 
-            var client = new RestClient($"https://api.openweathermap.org/data/2.5/weather?q={City}&appid=5705b52fdd12e0753d98f978798de52a");
+            var client = new RestClient($"https://www.udemy.com/api-2.0/courses/?page={numPages}&search={searchFor}");
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
             IRestResponse response = client.Execute(request);
 
-            var course = new UdemyCourse();
-
-            return course;
+            return ConvertResponseToUdemyCourse(response.Content, numPages);
         }
 
 
@@ -40,15 +38,30 @@ namespace team2backend.Controllers
             return Enumerable.Range(1, numPages).Select(index =>
 
             {
-                var jsonDailyForecast = json["daily"][index];
-                var unixDateTime = jsonDailyForecast.Value<long>("dt");
-                var weatherSummary = jsonDailyForecast["weather"][0].Value<string>("main");
+                var results = json["results"];
+                var instructor = results["visible_instructors"];
+                var jsonTitle = results.Value<string>("title");
+                var jsonUrl = results.Value<string>("url");
+                var jsonPrice = results.Value<string>("price");
+                var jsonCourseImage = results.Value<string>("image_240x135");
+                var jsonHeadline = results.Value<string>("headline");
+                var jsonInstructorTitle = instructor.Value<string>("job_title");
+                var jsonInstructorName = instructor.Value<string>("display_name");
+                var jsonInstructorPhoto = instructor.Value<string>("image_100x100");
 
-                return new WeatherForecast
+                return new UdemyCourse
                 {
-                    Date = DateTimeOffset.FromUnixTimeSeconds(unixDateTime).Date,
-                    TemperatureC = ExtractCelsiusTemperatureFromDailyWeather(jsonDailyForecast),
-                    Summary = weatherSummary
+                    Title = jsonTitle,
+                    Url = jsonUrl,
+                    Price = jsonPrice,
+                    CourseImage = jsonCourseImage,
+                    Headline = jsonHeadline,
+                    Instructor = new Instructor
+                    {
+                        Name = jsonInstructorName,
+                        Title = jsonInstructorTitle,
+                        Photo = jsonInstructorPhoto
+                    }
                 };
             })
             .ToArray();
