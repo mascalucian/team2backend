f3efc57fb5f729dc055152950621478675bf33a8
DirleaAndrei <78161390+DirleaAndrei@users.noreply.github.com>
2021-09-04 17:47:09 +0300

    Added ConvertResponseToUdemyCourseTest and tested.

--- team2backend.tests/team2backend.Tests/UdemyCourseControllerTest.cs
+++ team2backend.tests/team2backend.Tests/UdemyCourseControllerTest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using team2backend.Controllers;
 using Xunit;
 
 namespace team2backend.Tests
@@ -6,9 +8,32 @@ namespace team2backend.Tests
     public class UdemyCourseControllerTest
     {
         [Fact]
-        public void Test1()
+        public void ConvertResponseToUdemyCourseTest()
         {
+            // Assume
+            string content = LoadJsonFromResource("DataFromUdemyCoursesApi.json");
+            var controller = new UdemyCourseController();
 
+            // Act
+            var output = controller.ConvertResponseToUdemyCourse(content);
+
+            // Assert
+            var FirstCourse = ((UdemyCourse[])output)[0];
+            Assert.Equal("C Programming For Beginners - Master the C Language", FirstCourse.Title);
+            Assert.Equal("/course/c-programming-for-beginners-/", FirstCourse.Url);
+            Assert.Equal("€99.99", FirstCourse.Price);
+        }
+
+        private string LoadJsonFromResource(string jsonResponseOfApiFile)
+        {
+            var assembly = this.GetType().Assembly;
+            var assemblyName = assembly.GetName().Name;
+            var resourceName = $"{assemblyName}.{jsonResponseOfApiFile}";
+            var resourceStream = assembly.GetManifestResourceStream(resourceName);
+            using (var tr = new StreamReader(resourceStream))
+            {
+                return tr.ReadToEnd();
+            }
         }
     }
 }
