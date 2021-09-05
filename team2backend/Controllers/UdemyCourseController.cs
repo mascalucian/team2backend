3c6331de4247373ec5d9b14dcad87adba67b158b
DirleaAndrei <78161390+DirleaAndrei@users.noreply.github.com>
2021-09-05 22:32:02 +0300

    Updated Response and add error handlings for front end

--- team2backend/Controllers/UdemyCourseController.cs
+++ team2backend/Controllers/UdemyCourseController.cs
@@ -18,7 +18,7 @@ namespace team2backend.Controllers
         [EnableCors("CorsApi")]
         [HttpGet]
         [Route("{searchFor}/{numPage}")]
-        public IEnumerable<UdemyCourse> Get(string searchFor, int numPage)
+        public Response Get(string searchFor, int numPage)
         {
 
             var client = new RestClient($"https://www.udemy.com/api-2.0/courses/?page={numPage}&search={HttpUtility.UrlEncode(searchFor)}");
@@ -28,19 +28,62 @@ namespace team2backend.Controllers
             request.AddHeader("Authorization", _apiToken);
             IRestResponse response = client.Execute(request);
             var json = JObject.Parse(response.Content);
-            if (json.Value<long>("count") == 0)
+            var numOfCourses = json.Value<long>("count");
+            var lastPage = 0;
+            var numOfCoursesPerThisPage = 12;
+            //Set a value for lastPage
+            if (numOfCourses % 12 == 0)
             {
-                throw new ArgumentException(
-            $"No results found for {searchFor}.");
+                lastPage = (int)numOfCourses / 12;
             }
-            else return ConvertResponseToUdemyCourse(json);
+            else lastPage = (int)numOfCourses / 12 + 1;
+
+            if(numPage == lastPage)
+            {
+                numOfCoursesPerThisPage = (int)numOfCourses % 12;
+            }
+
+            if (json.Value<string>("detail") == "Invalid page." || json.Value<string>("detail") == "Invalid page size")
+            {
+                return GetResponseError(true, false, numOfCourses);
+            }
+            else if(json.Value<long>("count") == 0)
+            {
+                return GetResponseError(false, true, numOfCourses);
+            }
+            else return GetResponse(json, numOfCoursesPerThisPage, numOfCourses);
 
         }
 
         [NonAction]
-        public IEnumerable<UdemyCourse> ConvertResponseToUdemyCourse(JObject json)
+        public Response GetResponseError(bool wasOverFullFiled, bool noSearchFound, long numOfCourses)
         {
-            return Enumerable.Range(1, 12).Select(index =>
+            return new Response
+            {
+                WasOverFullFiled = wasOverFullFiled,
+                NoSearchFound = noSearchFound,
+                NumberOfCoursesFound = numOfCourses,
+                Courses = Enumerable.Empty<UdemyCourse>()
+            };
+        }
+
+
+    [NonAction]
+    public Response GetResponse(JObject json, int numOfCoursesPerThisPage, long numOfCourses)
+    {
+        return new Response
+        {
+            WasOverFullFiled = false,
+            NoSearchFound = false,
+            NumberOfCoursesFound = numOfCourses,
+            Courses = ConvertResponseToUdemyCourse(json, numOfCoursesPerThisPage)
+        };
+    }
+
+    [NonAction]
+        public IEnumerable<UdemyCourse> ConvertResponseToUdemyCourse(JObject json, int numOfCoursesPerThisPage)
+        {
+            return Enumerable.Range(1, numOfCoursesPerThisPage).Select(index =>
 
             {
                 var results = json["results"][index - 1];
---
+++ team2backend/Response.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace team2backend
+{
+    public class Response
+    {
+        public bool WasOverFullFiled { get; set; }
+
+        public bool NoSearchFound { get; set; }
+
+        public long NumberOfCoursesFound { get; set; }
+
+        public IEnumerable<UdemyCourse> Courses { get; set; }
+    }
+}
