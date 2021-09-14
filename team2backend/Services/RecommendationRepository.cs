5cf716172c5a91457b2c580258046a94820133bd
alXDraghiciu <88316605+alXDraghiciu@users.noreply.github.com>
2021-09-14 17:37:31 +0300

    created Recommendation and SKill Repositories

---
+++ team2backend/Services/RecommendationRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using team2backend.Data;
+using team2backend.Data.EfCore;
+using team2backend.Models;
+
+namespace team2backend.Services
+{
+    public class RecommendationRepository : EfCoreRepository<Recomandation, ApplicationDbContext>
+    {
+        public RecommendationRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ team2backend/Services/SkillRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using team2backend.Data;
+using team2backend.Data.EfCore;
+using team2backend.Models;
+
+namespace team2backend.Services
+{
+    public class SkillRepository : EfCoreRepository<Skill, ApplicationDbContext>
+    {
+        public SkillRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+    }
+}
