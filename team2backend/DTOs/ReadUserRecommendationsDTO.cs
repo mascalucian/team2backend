8540c4cc363a7891916e6d2b926c0f13ee7da2e6
DirleaAndrei <78161390+DirleaAndrei@users.noreply.github.com>
2021-09-18 23:44:57 +0300

    Added UserModel and ReadUserRecommendationDto

---
+++ team2backend/DTOs/ReadUserRecommendationsDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using team2backend.Models;
+
+namespace team2backend.DTOs
+{
+    public class ReadUserRecommendationsDTO
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public IEnumerable<Recomandation> Recommendations { get; set; }
+    }
+}
