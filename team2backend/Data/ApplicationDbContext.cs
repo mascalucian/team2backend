70e923c2a2a75eb2f057d5b2dbceea78f40f5c2e
DirleaAndrei <78161390+DirleaAndrei@users.noreply.github.com>
2021-09-18 17:27:30 +0300

    Updated dbContext

--- team2backend/Data/ApplicationDbContext.cs
+++ team2backend/Data/ApplicationDbContext.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using team2backend.Authentication;
 
 namespace team2backend.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -16,5 +14,10 @@ namespace team2backend.Data
         public DbSet<team2backend.Models.Skill> Skills { get; set; }
 
         public DbSet<team2backend.Models.Recomandation> Recomandations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
