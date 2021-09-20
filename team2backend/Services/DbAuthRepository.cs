8e753a94a9c22e05093b1e54040f291a9fdc1164
DirleaAndrei <78161390+DirleaAndrei@users.noreply.github.com>
2021-09-20 17:16:55 +0300

    Revert "Revert "Added Services for SkillsController""
    This reverts commit 2d4e51625a7b62608641d40ad80bdc34c9cc4bec.

---
+++ team2backend/Services/DbAuthRepository.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using AutoMapper.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using team2backend.Authentication;
+using team2backend.Authentication.Models;
+using team2backend.Dtos;
+using team2backend.Interfaces;
+
+namespace team2backend.Services
+{
+    public class DbAuthRepository : IAuthRepository
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly IConfiguration _configuration;
+
+        public DbAuthRepository(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+        public object Login(LoginModel model)
+        {
+            //var user = userManager.FindByNameAsync(model.Username);
+            //if (user != null && userManager.CheckPasswordAsync(user, model.Password))
+            //{
+            //    var userRoles = userManager.GetRolesAsync(user);
+
+            //    var authClaims = new List<Claim>
+            //    {
+            //        new Claim(ClaimTypes.Name, user.UserName),
+            //        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            //    };
+
+            //    foreach (var userRole in userRoles)
+            //    {
+            //        authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+            //    }
+
+            //    var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+            //    var token = new JwtSecurityToken(
+            //        issuer: _configuration["JWT:ValidIssuer"],
+            //        audience: _configuration["JWT:ValidAudience"],
+            //        expires: DateTime.Now.AddHours(3),
+            //        claims: authClaims,
+            //        signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256));
+            //    var readUserDto = new ReadUserDto
+            //    {
+            //        Id = user.Id,
+            //        UserName = user.UserName,
+            //        Email = user.Email,
+            //        Roles = userManager.GetRolesAsync(user).ConfigureAwait(false)
+            //    };
+            //    return new
+            //    {
+            //        token = new JwtSecurityTokenHandler().WriteToken(token),
+            //        expiration = token.ValidTo,
+            //        user = readUserDto,
+            //    };
+            //}
+
+            return new { };
+        }
+
+        public ResponseAuth Register(RegisterModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResponseAuth RegisterAdmin(RegisterModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ team2backend/Services/DbSkillRepository.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using team2backend.Controllers;
+using team2backend.Data;
+using team2backend.Dtos;
+using team2backend.Interfaces;
+using team2backend.Models;
+
+namespace team2backend.Services
+{
+    public class DbSkillRepository : ISkillsRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper mapper;
+
+        public DbSkillRepository(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            this.mapper = mapper;
+        }
+
+        public Skill CreateNewSkill(Skill skill)
+        {
+            var checkSkill = _context.Skills
+                   .FirstOrDefault(m => m.Name == skill.Name);
+            if (checkSkill != null) return null;
+            var content = UdemyCourseController.GetSearchResults(skill.Name, 1);
+            var json = JObject.Parse(content);
+            var numberOfCoursesPerSearch = json.Value<long>("count");
+            if (numberOfCoursesPerSearch == 0) return null;
+            _context.Add(skill);
+            _context.SaveChanges();
+            return skill;
+        }
+
+        public void Delete(int id)
+        {
+            var skill = _context.Skills.Find(id);
+            _context.Skills.Remove(skill);
+            _context.Recomandations.RemoveRange(_context.Recomandations.Where(_ => _.SkillId == id));
+            _context.Skills.RemoveRange(_context.Skills.Where(_ => _.ParentId == id));
+            _context.SaveChanges();
+        }
+
+        public Skill Edit(int id, Skill updatedSkill)
+        {
+            var skillToUpdate = _context.Skills.Find(id);
+
+            if (skillToUpdate != null)
+            {
+                skillToUpdate.Name = updatedSkill.Name;
+                var recommendations = _context.Recomandations.Where(_ => _.SkillId == id).ToList();
+                recommendations.ForEach(_ => _.SkillName = skillToUpdate.Name);
+                _context.Recomandations.UpdateRange(recommendations);
+                _context.SaveChanges();
+                return skillToUpdate;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<GetAllSkillsDto> GetAllSkills()
+        {
+            var skills = _context.Skills.ToList();
+            var getAllSkillsDto = mapper.Map<IEnumerable<GetAllSkillsDto>>(skills);
+            return getAllSkillsDto;
+        }
+
+        public GetSkillByIdDto GetSkillById(int id)
+        {
+            var skill = _context.Skills
+                    .FirstOrDefault(m => m.Id == id);
+            var getSkillById = mapper.Map<GetSkillByIdDto>(skill);
+            return getSkillById;
+        }
+    }
+}
