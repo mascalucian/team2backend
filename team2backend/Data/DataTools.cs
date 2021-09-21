e6f997848ad5699ea09c3cf440d5b61b7823a150
gramaovi <81247073+gramaovi@users.noreply.github.com>
2021-09-22 02:02:43 +0300

    Created DataTools for seed class

---
+++ team2backend/Data/DataTools.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace team2backend.Data
+{
+    public static class DataTools
+    {
+        public static void SeedData(this IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var applicationDbContext = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+            }
+        }
+    }
+}
