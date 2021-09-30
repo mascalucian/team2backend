3a44e32db035dc59820f3f9c93702c2d70dd43ad
DirleaAndrei <78161390+DirleaAndrei@users.noreply.github.com>
2021-09-30 23:32:57 +0300

    Revert "Revert "Create Model Roles for roles, which splits the roles in good and bad role""
    This reverts commit 01ff4192003ccf63ac144fba7d02c4efb6614b02.

--- team2backend/Controllers/UsersController.cs
+++ team2backend/Controllers/UsersController.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
-using team2backend.Authentication;
 using team2backend.Authentication.Models;
-using team2backend.Data;
 using team2backend.Dtos;
+using team2backend.Interfaces;
 using team2backend.Models;
 
 namespace team2backend.Controllers
@@ -19,139 +14,75 @@ namespace team2backend.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
-        private readonly ApplicationDbContext context;
-        private readonly UserManager<ApplicationUser> userManager;
-        private readonly RoleManager<IdentityRole> roleManager;
-        private readonly IMapper mapper;
         private readonly IHubContext<MessageHub> hubContext;
+        private readonly IUsersRepository usersRepository;
 
-        public UsersController(UserManager<ApplicationUser> userManager, IMapper mapper, RoleManager<IdentityRole> roleManager, ApplicationDbContext context, IHubContext<MessageHub> hubContext)
+        public UsersController(IHubContext<MessageHub> hubContext, IUsersRepository usersRepository)
         {
-            this.userManager = userManager;
-            this.mapper = mapper;
-            this.roleManager = roleManager;
-            this.context = context;
             this.hubContext = hubContext;
+            this.usersRepository = usersRepository;
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetUserById(string id)
         {
-            var user = await userManager.FindByIdAsync(id);
+            var user = await usersRepository.GetUserById(id);
             if (user != null)
             {
-                var userDto = mapper.Map<ApplicationUser, ReadUserDto>(user);
-                var roles = await userManager.GetRolesAsync(user).ConfigureAwait(false);
-                userDto.Roles = roles;
-                return Ok(userDto);
+                return Ok(user);
             }
-
-            return NotFound();
+            else
+            {
+                return NotFound();
+            }
         }
 
         [HttpPost]
         public async Task<IActionResult> AddNewUserWithRoles([FromBody] AddNewUser user)
         {
-            var userExists = await userManager.FindByEmailAsync(user.Email);
-            if (userExists != null)
+            var newUserWithRoles = await usersRepository.AddNewUserWithRoles(user);
+            if (newUserWithRoles.User == null)
             {
-                return StatusCode(StatusCodes.Status400BadRequest, new ResponseAuth { Status = "Error", Message = "User already exists!" });
-            }
-
-            ApplicationUser newUser = new()
-            {
-                Email = user.Email,
-                SecurityStamp = Guid.NewGuid().ToString(),
-                UserName = user.Email,
-            };
-
-            var badRoles = new List<string>();
-            var goodRoles = new List<string>();
-            foreach (var role in user.Roles)
-            {
-                if (!await roleManager.RoleExistsAsync(role))
-                {
-                    badRoles.Add(role);
-                }
-                else
-                {
-                    goodRoles.Add(role);
-                }
-            }
-
-            var result = await userManager.CreateAsync(newUser, user.Password);
-            if (!result.Succeeded)
-                return StatusCode(StatusCodes.Status400BadRequest, new ResponseAuth { Status = "Error", Message = "User creation failed! Please check user details and try again ." });
-
-            var badRolesInString = string.Empty;
-            foreach (var badRole in badRoles)
-            {
-                badRolesInString = badRolesInString + " " + badRole;
-            }
-
-            if (goodRoles.Count > 0)
-            {
-                foreach (var role in goodRoles)
-                {
-                    if (!await userManager.IsInRoleAsync(newUser, role))
-                    {
-                        await userManager.AddToRoleAsync(newUser, role);
-                    }
-                }
-            }
-            else
-            {
-                await userManager.AddToRoleAsync(newUser, UserRoles.User);
+                return BadRequest(newUserWithRoles.Response);
             }
 
             var userDto = new ReadUserDto()
             {
-                Id = newUser.Id,
-                UserName = newUser.UserName,
-                Email = newUser.Email,
-                Roles = goodRoles,
+                Id = newUserWithRoles.User.Id,
+                UserName = newUserWithRoles.User.UserName,
+                Email = newUserWithRoles.User.Email,
+                Roles = newUserWithRoles.GoodRoles,
             };
             await hubContext.Clients.All.SendAsync("UserAdded", userDto);
-            if (badRoles.Count == 0)
-            {
-                return Ok(new ResponseAuth { Status = "Success", Message = "User created successfully!" });
-            }
-            else
-            {
-                return Ok(new ResponseAuth { Status = "Success", Message = $"User created successfully, but roles {badRolesInString} are not valid!" });
-            }
+            return Ok(newUserWithRoles.Response);
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteUser(string id)
         {
-            var user = await userManager.FindByIdAsync(id);
+            var user = await usersRepository.DeleteUser(id);
             if (user != null)
             {
-                context.Recomandations.RemoveRange(context.Recomandations.Where(_ => _.UserId == id));
-                context.SaveChanges();
-                await userManager.DeleteAsync(user);
-                var userDto = mapper.Map<ReadUserDto>(user);
-                await hubContext.Clients.All.SendAsync("UserDeleted", userDto);
+                await hubContext.Clients.All.SendAsync("UserDeleted", user);
                 return Ok(new ResponseAuth { Status = "Success", Message = $"User {user.Email} was deleted !" });
             }
-
-            return NotFound();
+            else
+            {
+                return NotFound();
+            }
         }
 
         [HttpPost("{id}/roles")]
-        public async Task<IActionResult> AddRolesForUser(string id, [FromBody] string[] roles)
+        public async Task<IActionResult> EidtRolesForUser(string id, [FromBody] string[] roles)
         {
-            var user = await userManager.FindByIdAsync(id);
+            if (roles.Length == 0)
+            {
+                return BadRequest();
+            }
+
+            var user = await usersRepository.EidtRolesForUser(id, roles);
             if (user != null)
             {
-                if (roles == null || roles.Length == 0) return BadRequest();
-                var userRoles = await userManager.GetRolesAsync(user);
-                await userManager.RemoveFromRolesAsync(user, userRoles.ToArray());
-                foreach (var role in roles)
-                {
-                    await userManager.AddToRoleAsync(user, role);
-                }
                 var userDto = new ReadUserDto()
                 {
                     Id = user.Id,
@@ -160,28 +91,25 @@ namespace team2backend.Controllers
                     Roles = roles,
                 };
                 await hubContext.Clients.All.SendAsync("UserEdited", userDto);
-                return Ok();
+                return Ok(userDto);
             }
-
-            return NotFound();
+            else
+            {
+                return NotFound();
+            }
         }
-
 
         [HttpGet]
         public async Task<IActionResult> GetUsers()
         {
-            var task = Task.Run(() => userManager.Users.ToList());
-            var users = await task;
-            var usersDto = mapper.Map<List<ApplicationUser>, List<ReadUserDto>>(users);
-            foreach (var user in usersDto)
+            try
             {
-                var userTask = Task.Run(() => userManager.FindByIdAsync(user.Id));
-                var identityUser = await userTask;
-                var rolesTask = Task.Run(() => userManager.GetRolesAsync(identityUser));
-                user.Roles = await rolesTask;
+                return Ok(await usersRepository.GetUsers());
             }
-
-            return Ok(usersDto);
+            catch (Exception)
+            {
+                return BadRequest();
+            }
         }
     }
 }
---
+++ team2backend/Services/ServiceModels/Roles.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace team2backend.Services
+{
+    public class Roles
+    {
+        public List<string> GoodRoles { get; set; }
+
+        public List<string> BadRoles { get; set; }
+    }
+}
