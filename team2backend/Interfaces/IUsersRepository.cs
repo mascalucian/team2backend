e114c83959c7fcbeee95029b1b8580f86481f6b7
DirleaAndrei <78161390+DirleaAndrei@users.noreply.github.com>
2021-09-30 23:30:45 +0300

    Added IUsersRepository

---
+++ team2backend/Interfaces/IUsersRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using team2backend.Authentication;
+using team2backend.Dtos;
+using team2backend.Models;
+using team2backend.Services.ResponseModels;
+
+namespace team2backend.Interfaces
+{
+    public interface IUsersRepository
+    {
+        Task<AddNewUserResponse> AddNewUserWithRoles([FromBody] AddNewUser user);
+
+        Task<ApplicationUser> EidtRolesForUser(string id, [FromBody] string[] roles);
+
+        Task<ReadUserDto> DeleteUser(string id);
+
+        Task<ReadUserDto> GetUserById(string id);
+
+        Task<List<ReadUserDto>> GetUsers();
+    }
+}
